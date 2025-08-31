5482280b238323d86d0f2bf13fbcbb4e2975a6be
neerajching <neeraj.negi2307@gmail.com>
2025-08-31 12:35:26 +0530

    initial commit

---
+++ Pushka/PushkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushkaApp.swift
+//  Pushka
+//
+//  Created by Admin on 31/08/25.
+//
+
+import SwiftUI
+
+@main
+struct PushkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PushkaTests/PushkaTests.swift
@@ -0,0 +1,17 @@
+//
+//  PushkaTests.swift
+//  PushkaTests
+//
+//  Created by Admin on 31/08/25.
+//
+
+import Testing
+@testable import Pushka
+
+struct PushkaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
